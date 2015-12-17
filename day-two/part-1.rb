fa7da9ecd2584a3357cef2f6eb2ad138bd61a6e1
Russell <russellchristensen@me.com>
2015-12-17 17:47:25 +0000

    File read/write working

---
+++ day-two/part-1.rb
@@ -0,0 +1,12 @@
+# Get the input file and go line by line
+# Iterate through each line as an array
+# Separate each line by l w h
+#
+# Surface area: 2*l*w + 2*w*h + 2*h*l
+
+
+
+file = 'input.txt'
+File.readlines(file).each do |line|
+  puts line
+end
